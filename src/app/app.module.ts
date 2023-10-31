d85c9257988cc7a989d8886607ec600fcfbed578
Nche12 <ngangnche12@gmail.com>
2023-10-31 21:58:34 +0200

    mat toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { InventoryComponent } from './views/inventory/inventory.component';
+import { AnalyticsComponent } from './views/analytics/analytics.component';
+import { SupportComponent } from './views/support/support.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InventoryComponent,
+    AnalyticsComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
